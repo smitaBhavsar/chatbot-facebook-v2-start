c9802c1b209e37bb0b6fff0821d53c6b08e5ff5b
smita <smitabhavsar@gmail.com>
2019-01-31 14:49:30 +0530

    added webview code

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ if (!config.SERVER_URL) { //used for ink to static files
 
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 app.set('port', (process.env.PORT || 5000))
 
